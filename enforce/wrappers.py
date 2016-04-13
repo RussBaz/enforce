4c4e89f736b1043fc896d65c5285b89641aaa815
R Bazhenov <erusbl@gmail.com>
2016-04-13 20:40:36 +0600

    Added a sample implementation of ListProxy class and set 'wrapt' requirement to the latest version.

--- enforce/wrappers.py
+++ enforce/wrappers.py
@@ -1,3 +1,6 @@
+import typing
+from collections import UserList, MutableSequence
+
 from wrapt import ObjectProxy
 
 
@@ -59,3 +62,39 @@ class Proxy(ObjectProxy):
             self._self_pass_through = None
         else:
             del(self._self_pass_through)
+
+
+class ListProxy(ObjectProxy):
+    def __init__(self, input_validators: typing.List, output_validators: typing.List, wrapped: typing.List) -> None:
+        self._self_input_validators = input_validators
+        self._self_output_validators = output_validators
+        super().__init__(wrapped)
+    def __repr__(self): return self.__wrapped__.__repr__()
+    def __lt__(self, other): return self.__wrapped__.__lt__(other)
+    def __le__(self, other): return self.__wrapped__.__le__(other)
+    def __eq__(self, other): return self.__wrapped__.__eq__(other)
+    def __gt__(self, other): return self.__wrapped__.__gt__(other)
+    def __ge__(self, other): return self.__wrapped__.__ge__(other)
+    def __contains__(self, item): return self.__wrapped__.__contains__(item)
+    def __len__(self): return self.__wrapped__.__len__()
+    def __getitem__(self, i): return self.__wrapped__.__getitem__(i)
+    def __setitem__(self, i, item): return self.__wrapped__.__setitem__(i, item)
+    def __delitem__(self, i): return self.__wrapped__.__delitem__(i)
+    def __add__(self, other): return self.__wrapped__.__add__(other)
+    def __radd__(self, other): return self.__wrapped__.__radd__(other)
+    def __iadd__(self, other): return self.__wrapped__.__iadd__(other)
+    def __mul__(self, n): return self.__wrapped__.__mul__(n)
+    __rmul__ = __mul__
+    def __imul__(self, n): return self.__wrapped__.__imul__(n)
+
+    def append(self, item): self.__wrapped__.append(item)
+    def insert(self, i, item): self.__wrapped__.insert(i, item)
+    def pop(self, i=-1): return self.__wrapped__.pop(i)
+    def remove(self, item): self.__wrapped__.remove(item)
+    def clear(self): self.__wrapped__.clear()
+    def copy(self): return self.__wrapped__.copy()
+    def count(self, item): return self.__wrapped__.count(item)
+    def index(self, item, *args): return self.__wrapped__.index(item, *args)
+    def reverse(self): self.__wrapped__.reverse()
+    def sort(self, *args, **kwds): self.__wrapped__.sort(*args, **kwds)
+    def extend(self, other): self.__wrapped__.extend(other)
--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -1,12 +1,19 @@
 import unittest
 
+from wrapt import ObjectProxy
+
 from enforce import runtime_validation
-from enforce.wrappers import Proxy
+from enforce.wrappers import Proxy, ListProxy
 
 
 class WrappersTest(unittest.TestCase):
 
     def test_proxy_transparency(self):
+        """
+        Verifies if the proxy transparency can be switched on and off at runtime
+        If transparency is off, then if an attribute is changed, it is saved locally only
+        In such case a copy with '_self_' prefix is created on a proxy itself.
+        """
         class A:
             pass
 
@@ -35,17 +42,16 @@ class WrappersTest(unittest.TestCase):
         self.assertEqual(proxy_a.b, 3)
         self.assertEqual(proxy_a._self_b, 2)
 
-    @unittest.skip
-    def test_can_decorate(self):
-        class A:
-            def sample(self):
-                return data
+    def test_new(self):
+        a = [1, 2]
+        b = ListProxy(None, None, a)
+        b.append(3)
+        a.reverse()
 
-        a = A()
-        proxy_a = Proxy(a)
-        proxy_a.sample = runtime_validation(a.sample, a)
-
-        self.assertEqual(proxy_a.sample(1), 1)
+        self.assertEqual(a, b)
+        self.assertFalse(a is b)
+        self.assertTrue(isinstance(b, list))
+        self.assertTrue(isinstance(b, ObjectProxy))
 
 
 if __name__ == '__main__':
