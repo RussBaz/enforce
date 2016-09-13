67b14610d4e7fa1867f2e1719ae1c80817a8010f
RussBaz <RussBaz@users.noreply.github.com>
2016-09-13 23:32:27 +0100

    Updated the setup.py file for the 0.3.0 release.

--- setup.py
+++ setup.py
@@ -5,32 +5,29 @@ from setuptools import setup
 ROOT = path.abspath(path.dirname(__file__))
 
 with codecs.open(path.join(ROOT, 'README.md'), encoding='utf-8') as f:
-    LONG_DESCRIPTION = f.read()
+    LONG_DESCRIPTION = ''.join(f.readlines())
 
 setup(
     name='enforce',
 
-    version='0.2.0',
+    version='0.3.0',
 
-    description='A Python library for enforcing runtime validation of function inputs and outputs',
+    description='Python 3.5+ library for integration testing and data validation through configurable and optional runtime type hint enforcement.',
     long_description=LONG_DESCRIPTION,
 
-    # The project's main homepage.
     url='https://github.com/RussBaz/enforce',
-
-    # Author details
-    author='R. Bazhenov',
+    author='RussBaz',
     author_email='RussBaz@users.noreply.github.com',
 
     license='MIT',
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
     ],
-    keywords='type validation',
+    keywords='type validation type hints runtime type checking',
     packages=['enforce'],
     install_requires=[
         'wrapt'
